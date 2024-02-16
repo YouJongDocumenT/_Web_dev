67a72d33680f90b01146b46ed1a4e780fdbd66c1
YJDocumenT <yjdocument7@gmail.com>
2024-02-16 21:45:39 +0900

    SpringBoot 2.7 Base

---
+++ RAS/src/test/java/com/project/ras/RasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
