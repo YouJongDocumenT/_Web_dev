d12d77a89af1ee9dc9cfbe780263d358932e2835
YJDocumenT <yjdocument7@gmail.com>
2024-02-20 23:32:08 +0900

    메인 프론트 디자인

---
+++ RAS/src/main/java/com/project/ras/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.project.ras.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/main")
+    public String main(){
+        return "Main/MainPage";
+    }
+
+
+
+}
